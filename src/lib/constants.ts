df0875c2a4a4975a1715dfa4b10e0eab022208c7
Hameyalli <ache.mariana@gmail.com>
2025-12-04 17:49:21 -0600

    Update commit new UI

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -28,7 +28,7 @@ export const DEFAULT_CONFIGURATION: DefaultConfigurationType = {
   author: {
     avatar,
     name: 'Hame Elizalde',
-    headline: 'Product Designer & Digital Strategist | UX Writer especializada en educación y productos digitales de alto impacto.',
+    headline: 'Product Strategist | UX Writer especializada en educación y productos digitales',
     username: 'Ache_Zeta',
     location: 'México',
     pronouns: 'Ella/She',
